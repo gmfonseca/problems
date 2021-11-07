62205a51207c61e83391632311074910905f3cce
Gabriel Fonseca <gmagnofonseca@gmail.com>
2021-11-07 20:43:13 -0300

    Add coverage report

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.5.31"
+    jacoco
 }
 
 allprojects {
@@ -8,5 +9,40 @@ allprojects {
 
     repositories {
         mavenCentral()
+    }
+}
+
+// Setting jacoco
+
+jacoco {
+    toolVersion = "0.8.7"
+}
+
+tasks.register("codeCoverageReport", JacocoReport::class) {
+    subprojects subproject@{
+        apply(plugin = "jacoco")
+
+        plugins.withType(JacocoPlugin::class).configureEach {
+            tasks.matching { it.extensions.findByType<JacocoTaskExtension>() != null }.configureEach {
+                sourceSets(this@subproject.the<SourceSetContainer>()["main"])
+
+                if (file("${this@subproject.buildDir}/jacoco/test.exec").exists()) {
+                    executionData(this)
+                }
+            }
+
+            tasks.matching { it.extensions.findByType<JacocoTaskExtension>() != null }.forEach {
+                rootProject.tasks.getByName("codeCoverageReport").dependsOn(it)
+            }
+        }
+
+        tasks.withType(Test::class).configureEach {
+            finalizedBy(this@register) // TODO not working: report is always generated after tests run
+        }
+    }
+
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
     }
 }
